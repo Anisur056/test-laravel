18fb11714921a458a5ae5aacbe328d46b4039332
anisur rahman <ranisur056@gmail.com>
2025-07-22 20:18:03 +0600

    1.0.7

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.main')
 
 @section('page-title')
-    Laravel- Welcome
+    Laravel- about
 @endsection
 
 @section('body-content')
 
-    <h2>This is comming from `welcome.blade.php` page</h2>
+    <h2>This is comming from `about.blade.php` page</h2>
 
 @endsection
